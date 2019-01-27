45b3f37294d43c62ff8b7a8c17b899d9c74f079a
Srinjan Ghosh <srinjanghosh11@gmail.com>
2019-01-28 01:32:49 +0530

    added the login(design not complete)

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -1,1 +1,19 @@
-this is login
+<div class="container">
+    <div class="heading">
+        <h1>Login</h1>
+    </div>
+    <div class="form-group form-element">
+        <label>Username</label>
+        <div class="input">
+            <i class="far fa-user"></i>
+            <input type="text" name="username" placeholder= "Type your Username">
+        </div>
+    </div>
+    <div class="form-group form-element">
+        <label>Password</label>
+        <div class="input">
+            <i class="fas fa-lock"></i>
+            <input type="text" name="password" placeholder= "Type your Password">
+        </div>
+    </div>
+</div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,5 +6,7 @@
     <title><?php echo $title ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/static/css/login.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 </head>
 <body>
